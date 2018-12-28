af0dc3637da5c9149d7d842eff8ababf1c35b5b4
lpeltonjr <lpeltonjr@bellsouth.net>
2018-12-28 10:42:49 -0600

    style sheet mods

--- src/App.js
+++ src/App.js
@@ -2,24 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function AppHeader(props) {
+  return (
+    <header className="page-header">
+      <span className="page-title-icon"><i className="fa fa-bars page-title-text"></i></span>
+      <div className="page-title page-title-text">{props.headerText}</div>
+    </header>
+  );
+}
+
+function AppMap(props) {
+  return (<div className="map-col">test</div>);
+}
+
+function AppFilter(props) {
+  return (<div className="fixed-filter-col">test</div>);
+}
+
+function AppBody(props) {
+  return (
+    <div className="page-content">
+      <AppFilter />
+      <AppMap />
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="page-container">
+        <AppHeader headerText="GERMANTOWN PLACES" />        
+        <AppBody />
       </div>
     );
   }
